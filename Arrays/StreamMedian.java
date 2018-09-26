4a5a0dbe5279a34c026df73ecad04436124efad3
Abhishek <ab96sh@gmail.com>
2018-09-26 21:15:57 +0530

    Created StreamMedian.java

---
+++ Arrays/StreamMedian.java
@@ -0,0 +1,144 @@
+import java.util.*;
+import java.io.*;
+
+public class Template {
+
+	public static void main(String[] args){
+		ShortScan sc = new ShortScan();
+		int n = sc.ni();
+		int[] arr = sc.narr(n);
+		
+		PriorityQueue<Integer> highers = new PriorityQueue<>();
+		PriorityQueue<Integer> lowers =
+		    new PriorityQueue<>(Collections.reverseOrder());
+		
+		/*
+		Three steps:
+		-> Add element to either of the heaps
+		-> Rebalance the heaps
+		-> Get median
+		*/
+		
+		for(int x: arr){
+		    addElement(x, highers, lowers);
+		    rebalanceHeaps(highers, lowers);
+		    po(getMedian(highers, lowers));
+		  //  po(highers);
+		  //  po(lowers);
+		  //  po(" ");
+		}
+	}
+	
+	public static void addElement(int ele, PriorityQueue<Integer> highers,
+	            PriorityQueue<Integer> lowers){
+	    
+	    if(lowers.size()==0 || ele < lowers.peek()){
+	        lowers.add(ele);
+	    } else {
+	        highers.add(ele);
+	    }
+	}
+	
+	public static void rebalanceHeaps(PriorityQueue<Integer> highers,
+	            PriorityQueue<Integer> lowers){
+	    /*
+	    Determine which heap has more elements, and whether the diff.
+	    between the size of the heaps is more than 1.
+	    */
+        PriorityQueue<Integer> moreSize = 
+            (highers.size() > lowers.size()) ? highers : lowers;
+        PriorityQueue<Integer> lessSize = 
+            (highers.size() < lowers.size()) ? highers : lowers;
+            
+        if(moreSize.size() - lessSize.size() > 1){
+            // po("REb");
+            lessSize.add(moreSize.poll());
+        }
+	}
+	
+	public static int getMedian(PriorityQueue<Integer> highers,
+	            PriorityQueue<Integer> lowers){
+	    /*
+	    If size of both heaps is same, then the median is the average 
+	    of the values of top values of both the heaps.
+	    
+	    Otherwise the median is either the top value of highers
+	    or lowers, whichever is bigger.
+	    */
+	    if(lowers.size()==0) return highers.peek();
+	    if(highers.size()==0) return lowers.peek();
+	    
+	    /*
+	    if(highers.size() == lowers.size()){
+	        return (highers.peek()+lowers.peek())/2;
+	    } else {
+	        return (highers.peek() < lowers.peek())? highers.peek()
+	                :  lowers.peek();
+	    }*/
+	    
+	    if(highers.size() == lowers.size()){
+	        return (highers.peek()+lowers.peek())/2;
+	    } else {
+	        return (highers.size() > lowers.size()) ? highers.peek() : lowers.peek();
+	    }
+	}
+
+
+//**********************************************************************//	
+
+	public static void po(Object o){
+		System.out.println(o);
+	}
+
+	public static void p(Object o){
+		System.out.print(o);
+	}
+
+	static class ShortScan {
+			Scanner sc;
+
+			public ShortScan(){
+				sc = new Scanner (System.in);
+			}
+
+			public int ni(){
+				return sc.nextInt();
+			}
+
+			public long nl(){
+				return sc.nextLong();
+			}
+
+			public String n(){
+				return sc.next();
+			}
+
+			public String ns(){
+				return sc.nextLine();
+			}
+
+			public int[] narr(int n){
+				int[] arr = new int[n];
+				for(int i=0; i<arr.length; i++){
+					arr[i] = sc.nextInt();
+				}
+				return arr;
+			}
+	}
+
+	public static <T> void showArr(T[] arr){
+		for(T x: arr){
+			p(x+" ");
+		}
+		po("");
+	}
+
+	public static void show2d(int[][] arr){
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr.length; j++){
+				p(arr[i][j]+" ");
+			}
+			po("");
+		}
+	}
+}	
