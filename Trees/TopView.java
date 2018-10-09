bf64c7695e633632cdc14b6bc122d88e1e448890
Abhishek <ab96sh@gmail.com>
2018-10-09 18:39:50 +0530

    Update TopView.java

--- Trees/TopView.java
+++ Trees/TopView.java
@@ -23,12 +23,14 @@ can't be seen. (From the top, only one node of a particular dis can be seen).
     Output: 2 1 3 6 (4 and 5 get obstructed from the view bcz of 1 and 3).
 */
 
+/*
 Algo:
   The recursion is almost the same as that of vertical traversal, however, here we keep track of current level also
   while storing the node at a particular distance. (We store the node as well as it's level for a particular dis.)
   If another node of same distance is encountered, we check if the current level is less or more than that of prev
   stored node at that dist. If the level is less, then we replace it. Otherwise not.
-    
+ */
+
     class Pair {
           int data;
           int level;
